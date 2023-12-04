f2fa95ad643d0d41ff68a22c2a7ced959baa8e95
debeule <debeulematthias@gmail.com>
2023-12-04 16:55:45 +0100

    settings linux setup

--- bp_articles_scraper/settings.py
+++ bp_articles_scraper/settings.py
@@ -1,5 +1,6 @@
 import os
-from dotenv import load_dotenv  
+from dotenv import load_dotenv 
+from shutil import which 
 
 load_dotenv()
 
@@ -7,16 +8,16 @@ BOT_NAME = "bp_articles_scraper"
 
 SPIDER_MODULES = ["bp_articles_scraper.spiders"]
 NEWSPIDER_MODULE = "bp_articles_scraper.spiders"
-LOG_FILE = 'scrapy.log'
 
-ROBOTSTXT_OBEY = False
+# LOG_FILE = 'scrapy.log'
+LOG_LEVEL = 'INFO'
 
-REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
-TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+TWISTED_REACTOR = "twisted.internet.epollreactor.EPollReactor"
 FEED_EXPORT_ENCODING = "utf-8"
 
 DOWNLOADER_MIDDLEWARES = {
     'bp_articles_scraper.middlewares.proxy_middleware.ProxyMiddleware': 100,
+    'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110,
     'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 200,
     'bp_articles_scraper.middlewares.selenium_middleware.SeleniumMiddleware': 300,
 }
