f3b3d73834b3d158472d6b7a134e7b83e4fddec3
debeule <debeulematthias@gmail.com>
2024-01-29 11:36:02 +0100

    Update to browseroptions / added extensions to options

--- bp_articles_scraper/middlewares/options_middleware.py
+++ bp_articles_scraper/middlewares/options_middleware.py
@@ -1,25 +1,32 @@
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+
 
 
 class OptionsMiddleware:
     
     def __init__(self):
-        self.options = Options()
+        self.options = ChromeOptions()
         
-        self.options.add_argument('--headless')
-        self.options.add_argument("start-maximized")
+        self.options.add_argument('--headless=new')
+        # self.options.add_argument("start-maximized")
         self.options.add_argument('--no-sandbox')
+
         #use tmp instead of /dev/shm (not ideal because meomory ==> disk)
         self.options.add_argument('--disable-dev-shm-usage')
+
+        self.options.add_argument('--disable-javascript')
+        self.options.add_argument('--disable-images')
 
         self.options.add_experimental_option("excludeSwitches", ["enable-automation"])
         self.options.add_experimental_option('useAutomationExtension', False)
 
         
-        extension_path = '/usr/src/app/extensions/bypass-paywalls-chrome-master/'
-        self.options.add_argument(f'--load-extension={extension_path}')
-
-
+        extensions_path = '/app/extensions/'
+        # self.options.add_argument('load-extension=/app/extensions/bypass-paywalls-chrome-master')
+        self.options.add_extension(extensions_path + 'bypass-paywalls.crx')
+        self.options.add_extension(extensions_path + 'add-block-plus.crx')
+        # self.options.add_extension(extensions_path + 'request-blocker.crx')
 
     def process_request(self, request, spider):
 
