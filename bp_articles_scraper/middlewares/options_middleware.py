3e6df3589382140dea719121fe84cdf45aad33e2
debeule <debeulematthias@gmail.com>
2024-05-06 22:36:51 +0200

    Added options for saving data

--- bp_articles_scraper/middlewares/options_middleware.py
+++ bp_articles_scraper/middlewares/options_middleware.py
@@ -16,17 +16,23 @@ class OptionsMiddleware:
         self.options.add_argument('--disable-dev-shm-usage')
 
         self.options.add_argument('--disable-javascript')
+        
         self.options.add_argument('--disable-images')
+        self.options.add_argument("--blink-settings=imagesEnabled=false")
+
+        #compresses data to save bandwidth
+        self.options.add_argument("--enable-data-reduction-proxy-fallback")
+        
 
         self.options.add_experimental_option("excludeSwitches", ["enable-automation"])
         self.options.add_experimental_option('useAutomationExtension', False)
 
+
         
         extensions_path = '/code/extensions/'
-        # self.options.add_argument('load-extension=/app/extensions/bypass-paywalls-chrome-master')
+        
         self.options.add_extension(extensions_path + 'bypass-paywalls.crx')
         self.options.add_extension(extensions_path + 'add-block-plus.crx')
-        # self.options.add_extension(extensions_path + 'request-blocker.crx')
 
     def process_request(self, request, spider):
 
